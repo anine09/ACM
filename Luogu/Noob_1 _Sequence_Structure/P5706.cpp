6e8e918f029689e20ae43d125064809cc0873c83
epsilon_luoo <epsilon_luoo@outlook.com>
2021-09-07 18:40:23 +0800

    update

--- Luogu/Noob_1 _Sequence_Structure/P5706.cpp
+++ Luogu/Noob_1 _Sequence_Structure/P5706.cpp
@@ -7,7 +7,14 @@ int n;
 int main(){
 
     scanf("%f %d", &t, &n);
-    printf("%f %d", t, n);
+
+    int cup;
+    float milliliter;
+
+    cup = n * 2;
+    milliliter = t / n;
+
+    printf("%.3f\n%d", milliliter, cup);
 
     return 0;
 }
