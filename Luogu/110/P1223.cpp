820cf3aa251f3b25e78130133aa08f3d01131f9a
epsilon_luoo <epsilon_luoo@outlook.com>
2021-11-17 16:18:42 +0800

    update

--- Luogu/110/P1223.cpp
+++ Luogu/110/P1223.cpp
@@ -1,3 +1,4 @@
+
 #include <algorithm>
 #include <cstdio>
 #include <iostream>
@@ -18,7 +19,7 @@ int main()
     cout << endl;
     cout << "-------------------------";
     cout << endl;
-    sort(T, T + n);
+    sort(T, T + n+1);
     for (int j = 1; j <= n; j++)
     {
         for (int m = 1; m <= n; m++)
@@ -37,3 +38,42 @@ int main()
 
     return 0;
 }
+
+
+
+/*
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+struct node
+{
+    int n,t; 
+} ;
+struct node t[1010];
+bool cmp(node x,node y)
+{
+    return x.t<y.t;
+}
+int main()
+{
+    int n,sum=0,i;
+    double ans;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&t[i].t);
+        t[i].n=i+1;
+    }
+    sort(t,t+n,cmp);
+    int j=n-1;
+    for(i=0;i<n;i++)
+    {
+        ans=ans+j*t[i].t;
+        printf("%d ",t[i].n);
+        j--;
+    }
+    printf("\n");
+    printf("%.2f",ans/n);
+    return 0;
+}
+*/
