803ccf108aeb2a462ff30a720e9c6258fe79cf93
epsilon_luoo <epsilon_luoo@outlook.com>
2021-12-06 02:22:04 +0800

    update

--- Luogu/107/P1012.cpp
+++ Luogu/107/P1012.cpp
@@ -1,32 +1,38 @@
-#include<iostream>
-#include<string>
-#include<algorithm>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <string>
 using namespace std;
 
-struct num{
+struct num
+{
     string s;
-    int the_num;
-}s[30];
+    int first_num;
+} s[30];
+
 int n;
 
-bool cmp(num a,num b){
-    int a_num = a.s[0]
+bool cmp(num a, num b)
+{
+
+    if (a.first_num == b.first_num)
+        return *(a.s.end() - 1) < *b.s.begin();
     return a.first_num > b.first_num;
 }
 
-int main(){
-    cin>>n;
+int main()
+{
+    cin >> n;
     for (int i = 0; i < n; i++)
     {
         cin >> s[i].s;
-        
+        s[i].first_num = s[i].s[0];
     }
     sort(s, s + n, cmp);
     for (int i = 0; i < n; i++)
     {
         cout << s[i].s;
     }
-    
 
     return 0;
 }
