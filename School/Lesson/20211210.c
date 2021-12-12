05c3b6b5d699cd92eb097adb5f1379372567cfa7
epsilon_luoo <epsilon_luoo@outlook.com>
2021-12-12 12:43:50 +0800

    update

---
+++ School/Lesson/20211210.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int max;
+int min;
+int main(void)
+{
+    int sum = 0;
+    int n[10000];
+    int num;
+    while (1)
+    {
+        scanf("%d", &num);
+        if (num == 0)
+            break;
+        
+            n[sum] = num;
+        sum++;
+    }
+    max = min = n[0];
+    for(int i = 0; i < sum;i++)
+{
+    if(n[i]>max)
+        max = n[i];
+    if(n[i]<min)
+        min = n[i];
+}
+printf("min = %d\n",min);
+printf("max = %d\n",max);
+return 0;
+}
+n0 n1 n2 n3
+1 2    3 4 
